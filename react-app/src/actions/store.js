25ab5225348664969cd06614196e82360314f525
valentynkit14 <valentinkit14@gmail.com>
2021-12-21 19:15:34 +0200

    setting react-redux

---
+++ react-app/src/actions/store.js
@@ -0,0 +1,8 @@
+import { createStore,applyMiddleware,compose } from "redux";
+import thunk from "redux-thunk";
+export const store = createStore(
+    {},
+    compose(
+        applyMiddleware(thunk)
+    )
+)
