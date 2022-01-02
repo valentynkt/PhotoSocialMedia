818bb6332e26bdde4e0952b75a8b1923165f9224
valentynkit14 <valentinkit14@gmail.com>
2022-01-02 18:35:02 +0200

    registered useState 2.0

--- react-app/src/components/Register.js
+++ react-app/src/components/Register.js
@@ -2,6 +2,7 @@ import React, { Component, useState } from "react";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 import Guest from "./Guest";
+import { Alert } from "react-bootstrap";
 const Register = () => {
   const [error, setError] = useState(null);
   const [user, setUser] = useState({
@@ -12,6 +13,7 @@ const Register = () => {
     email: "",
     phone: "",
     password: "",
+    confirmPassword:""
   });
   const [registered, setRegistered] = useState(false);
 
@@ -22,45 +24,53 @@ const Register = () => {
     setUser({ ...user, secondName: event.target.value });
   };
   const handleAbout = (event) => {
-    setUser({ ...user, firstName: event.target.value });
+    setUser({ ...user, about: event.target.value });
   };
   const handleGender = (event) => {
-    setUser({ ...user, secondName: event.target.value });
+    setUser({ ...user, gender: event.target.value });
   };
   const handleEmail = (event) => {
-    setUser({ ...user, firstName: event.target.value });
+    setUser({ ...user, email: event.target.value });
   };
   const handlePhone = (event) => {
-    setUser({ ...user, secondName: event.target.value });
+    setUser({ ...user, phone: event.target.value });
   };
   const handlePassword = (event) => {
-    setUser({ ...user, secondName: event.target.value });
+    setUser({ ...user, password: event.target.value });
   };
-
+  const handleConfirmPassword = (event) => {
+    setUser({ ...user, confirmPassword: event.target.value });
+  };
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = {
-      FirstName: user.firstName,
-      SecondName: user.lastName,
-      About: user.about,
-      Gender: user.gender,
-      Email: user.email,
-      Phone: user.phone,
-      Password: user.password,
-    };
-    try {
-      await axios.post("User/signup", data);
-      setRegistered(true);
-      setError(null);
-    } catch (err) {
-      if (err.response.status === 401 || err.response.status === 400) {
-        setError(err.response.data.message);
-      } else {
-        setError("something went wrong. Please try again");
-      }
-      console.log("error >>>", err);
+    setError(null);
+    if (user.password!==user.confirmPassword) {
+      setError("Confirm password and password are different")
     }
+    else{
+      const data = {
+        FirstName: user.firstName,
+        SecondName: user.lastName,
+        About: user.about,
+        Gender: user.gender,
+        Email: user.email,
+        Phone: user.phone,
+        Password: user.password,
+      };
+      try {
+        await axios.post("User/signup", data);
+        setRegistered(true);
+      } catch (err) {
+        if (err.response.status === 401 || err.response.status === 400) {
+          setError(err.response.data.message);
+        } else {
+          setError("something went wrong. Please try again");
+        }
+        console.log("error >>>", err);
+      }
+    } 
   };
   if (registered) {
     return <Navigate to={"/login"} />;
@@ -71,44 +81,113 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
           <h3>Register</h3>
 
-          <div className="form-group">
-            <label>First Name</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="First Name"
-              onChange={(e) => (this.firstName = e.target.value)}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Last Name</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Last Name"
-              onChange={(e) => (this.lastName = e.target.value)}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Email</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Email"
-              onChange={(e) => (this.email = e.target.value)}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Password"
-              onChange={(e) => (this.password = e.target.value)}
-            />
+          <div class="card-body">
+                <div class="row gutters">
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 class="mb-2 text-primary">Personal Details</h6>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="firstName">First Name</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="firstName"
+                        placeholder="Enter first name"
+                        onChange={handleFirstName}
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="lastName">Last Name</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="lastName"
+                        placeholder="Enter last name"
+                        onChange={handleSecondName}
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="eMail">Email</label>
+                      <input
+                        type="email"
+                        class="form-control"
+                        id="eMail"
+                        placeholder="Enter email"
+                        onChange={handleEmail}
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="phone">Phone</label>
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="phone"
+                        placeholder="Enter phone number"
+                        onChange={handlePhone}
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="about">About</label>
+                      <textarea
+                        class="form-control"
+                        id="about"
+                        placeholder="Write About Yourself"
+                        required
+                        onChange={handleAbout}
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="gender">Gender</label>
+                      <select class="form-select  mb-3"  onChange={handleGender}>
+                        <option defaultValue value="male">
+                          Male
+                        </option>
+                        <option value="female">Female</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="row gutters">
+                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 class="mt-3 mb-2 text-primary">Password</h6>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="Street">Password</label>
+                      <input
+                        type="name"
+                        class="form-control"
+                        id="Street"
+                        placeholder="Enter Street"
+                        onChange={handlePassword}
+                      />
+                    </div>
+                  </div>
+                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div class="form-group">
+                      <label for="ciTy">Confirm Password</label>
+                      <input
+                        type="name"
+                        class="form-control"
+                        id="ciTy"
+                        placeholder="Enter City"
+                        onChange={handleConfirmPassword}
+                      />
+                    </div>
+                  </div>
+                </div>
+                {error && <Alert severity="error">{error}</Alert>}
           </div>
           <button className="btn btn-primary btn-block btn-lg">Sign Up</button>
         </form>
