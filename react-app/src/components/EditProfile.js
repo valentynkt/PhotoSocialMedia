9600899764226f557bbe2903877a67e9a0bac988
valentynkit14 <valentinkit14@gmail.com>
2022-01-03 18:57:43 +0200

    edit profile working version

--- react-app/src/components/EditProfile.js
+++ react-app/src/components/EditProfile.js
@@ -3,21 +3,23 @@ import User from "./User";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 import { Alert } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 function EditProfile() {
   let user = JSON.parse(localStorage.getItem("user"));
+  const navigate = useNavigate();
   const [error, setError] = useState(null);
   const [newUser, setNewUser] = useState({
-    firstName: user.firstName,
-    secondName: user.secondName,
-    about: user.about,
-    gender: user.gender,
-    email: user.email,
-    phone: user.phoneNumber,
+    firstName: user ? user.firstName : "",
+    secondName: user ? user.secondName : "",
+    about: user ? user.about : "",
+    gender: user ? user.gender: "",
+    email: user ? user.email: "",
+    phone: user ? user.phoneNumber: "",
     password: "",
     confirmPassword: "",
   });
-  const [redirect, setRedirect] = useState(false);
 
+  const [redirect, setRedirect] = useState(null);
   const handleChangeFunc = (event) => {
     const { name, value } = event.target;
 
@@ -34,16 +36,17 @@ function EditProfile() {
       setError("Confirm password and password are different");
     } else {
       const data = {
-        FirstName: user.firstName,
-        SecondName: user.secondName,
-        About: user.about,
-        Gender: user.gender,
-        Password: user.password,
+        FirstName: newUser.firstName,
+        SecondName: newUser.secondName,
+        About: newUser.about,
+        Gender: newUser.gender,
+        Email: newUser.email,
+        Password: newUser.password,
       };
       try {
         await axios.put("User", data);
         localStorage.clear();
-        setRedirect(true);
+        setRedirect("/login");
       } catch (err) {
         if (err.response.status === 401 || err.response.status === 400) {
           setError(err.response.data.message);
@@ -54,25 +57,30 @@ function EditProfile() {
       }
     }
   };
+
+  const cancelledHandler = () => {
+    setRedirect("/user/account");
+  };
+
   if (redirect) {
-    return <Navigate to={"/login"} />;
+    return <Navigate to={redirect} />;
   }
 
   return (
     <User>
       <div className="auth-inner">
-        <div class="row gutters">
-          <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
-            <div class="card h-100">
-              <div class="card-body">
-                <div class="account-settings">
-                  <div class="user-profile">
-                    <h5 class="user-name">
+        <div className="row gutters">
+          <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+            <div className="card h-100">
+              <div className="card-body">
+                <div className="account-settings">
+                  <div className="user-profile">
+                    <h5 className="user-name">
                       {user.firstName + " " + user.secondName}
                     </h5>
-                    <h6 class="user-email">{user.email}</h6>
+                    <h6 className="user-email">{user.email}</h6>
                   </div>
-                  <div class="about">
+                  <div className="about">
                     <h5>About</h5>
                     <p>
                       I'm Yuki. Full Stack Designer I enjoy creating
@@ -83,48 +91,48 @@ function EditProfile() {
               </div>
             </div>
           </div>
-          <div class="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
-            <div class="card h-100">
-              <div class="card-body">
-                <div class="row gutters">
-                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <h6 class="mb-2 text-primary">Personal Details</h6>
+          <div className="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+            <div className="card h-100">
+              <div className="card-body">
+                <div className="row gutters">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 className="mb-2 text-primary">Personal Details</h6>
                   </div>
-                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="form-group">
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
                       <label for="firstName">First Name</label>
                       <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
                         id="firstName"
                         placeholder="Enter first name"
                         name="firstName"
                         required
-                        minlength="3"
+                        minLength="3"
                         onChange={handleChangeFunc}
                       />
                     </div>
                   </div>
-                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="form-group">
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
                       <label for="lastName">Last Name</label>
                       <input
                         type="text"
-                        class="form-control"
+                        className="form-control"
                         id="lastName"
                         placeholder="Enter last name"
                         name="secondName"
                         required
-                        minlength="3"
+                        minLength="3"
                         onChange={handleChangeFunc}
                       />
                     </div>
                   </div>
-                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="form-group">
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
                       <label for="about">About</label>
                       <textarea
-                        class="form-control"
+                        className="form-control"
                         id="about"
                         placeholder="Write About Yourself"
                         name="about"
@@ -134,11 +142,11 @@ function EditProfile() {
                       ></textarea>
                     </div>
                   </div>
-                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="form-group">
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
                       <label for="gender">Gender</label>
                       <select
-                        class="form-select  mb-3"
+                        className="form-select  mb-3"
                         name="gender"
                         onChange={handleChangeFunc}
                       >
@@ -150,16 +158,16 @@ function EditProfile() {
                     </div>
                   </div>
                 </div>
-                <div class="row gutters">
-                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <h6 class="mt-3 mb-2 text-primary">Password</h6>
+                <div className="row gutters">
+                  <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h6 className="mt-3 mb-2 text-primary">Password</h6>
                   </div>
-                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="form-group">
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
                       <label for="Password">Password</label>
                       <input
                         type="password"
-                        class="form-control"
+                        className="form-control"
                         id="Password"
                         placeholder="Enter Password"
                         name="password"
@@ -169,12 +177,12 @@ function EditProfile() {
                       />
                     </div>
                   </div>
-                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="form-group">
+                  <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                    <div className="form-group">
                       <label for="ConfirmPassword">Confirm Password</label>
                       <input
                         type="password"
-                        class="form-control"
+                        className="form-control"
                         id="ConfirmPassword"
                         placeholder="Confirm Password"
                         name="confirmPassword"
@@ -186,13 +194,14 @@ function EditProfile() {
                   </div>
                 </div>
                 {error && <Alert severity="error">{error}</Alert>}
-                <div class="row gutters d-flex justify-content-center">
-                  <div class="d-flex justify-content-around updateBlockButtons">
+                <div className="row gutters d-flex justify-content-center">
+                  <div className="d-flex justify-content-around updateBlockButtons">
                     <button
                       type="button"
                       id="submit"
                       name="submit"
-                      class="btn btn-secondary btn-lg"
+                      className="btn btn-secondary btn-lg"
+                      onClick={cancelledHandler}
                     >
                       Cancel
                     </button>
@@ -200,12 +209,13 @@ function EditProfile() {
                       type="button"
                       id="submit"
                       name="submit"
-                      class="btn btn-primary btn-lg"
+                      className="btn btn-primary btn-lg"
+                      onClick={handleSubmit}
                     >
                       Update
                     </button>
                   </div>
-              </div>
+                </div>
               </div>
             </div>
           </div>
