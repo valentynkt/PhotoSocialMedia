9a2df216f70e6e23780a27d4dee10bdf063f8798
valentynkit14 <valentinkit14@gmail.com>
2022-01-03 17:11:19 +0200

    edit profile layout

--- react-app/src/components/EditProfile.js
+++ react-app/src/components/EditProfile.js
@@ -1,5 +1,8 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import User from "./User";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+import { Alert } from "react-bootstrap";
 function EditProfile() {
   let user = JSON.parse(localStorage.getItem("user"));
   const [error, setError] = useState(null);
@@ -11,38 +14,36 @@ function EditProfile() {
     email: user.email,
     phone: user.phoneNumber,
     password: "",
-    confirmPassword:""
+    confirmPassword: "",
   });
-  const [registered, setRegistered] = useState(false);
+  const [redirect, setRedirect] = useState(false);
 
   const handleChangeFunc = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
     setNewUser((prevValue) => ({
-        ...prevValue,
-        [name]: value,
-    }))
-}
+      ...prevValue,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(null);
-    if (user.password!==user.confirmPassword) {
-      setError("Confirm password and password are different")
-    }
-    else{
+    if (user.password !== user.confirmPassword) {
+      setError("Confirm password and password are different");
+    } else {
       const data = {
         FirstName: user.firstName,
         SecondName: user.secondName,
         About: user.about,
         Gender: user.gender,
-        Email: user.email,
-        PhoneNumber: user.phone,
         Password: user.password,
       };
       try {
-        await axios.post("User/signup", data);
-        setRegistered(true);
+        await axios.put("User", data);
+        localStorage.clear();
+        setRedirect(true);
       } catch (err) {
         if (err.response.status === 401 || err.response.status === 400) {
           setError(err.response.data.message);
@@ -51,9 +52,9 @@ function EditProfile() {
         }
         console.log("error >>>", err);
       }
-    } 
+    }
   };
-  if (registered) {
+  if (redirect) {
     return <Navigate to={"/login"} />;
   }
 
@@ -97,6 +98,10 @@ function EditProfile() {
                         class="form-control"
                         id="firstName"
                         placeholder="Enter first name"
+                        name="firstName"
+                        required
+                        minlength="3"
+                        onChange={handleChangeFunc}
                       />
                     </div>
                   </div>
@@ -108,28 +113,10 @@ function EditProfile() {
                         class="form-control"
                         id="lastName"
                         placeholder="Enter last name"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="form-group">
-                      <label for="eMail">Email</label>
-                      <input
-                        type="email"
-                        class="form-control"
-                        id="eMail"
-                        placeholder="Enter email"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                    <div class="form-group">
-                      <label for="phone">Phone</label>
-                      <input
-                        type="text"
-                        class="form-control"
-                        id="phone"
-                        placeholder="Enter phone number"
+                        name="secondName"
+                        required
+                        minlength="3"
+                        onChange={handleChangeFunc}
                       />
                     </div>
                   </div>
@@ -140,14 +127,21 @@ function EditProfile() {
                         class="form-control"
                         id="about"
                         placeholder="Write About Yourself"
+                        name="about"
                         required
+                        minLength="6"
+                        onChange={handleChangeFunc}
                       ></textarea>
                     </div>
                   </div>
                   <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
                       <label for="gender">Gender</label>
-                      <select class="form-select  mb-3">
+                      <select
+                        class="form-select  mb-3"
+                        name="gender"
+                        onChange={handleChangeFunc}
+                      >
                         <option defaultValue value="male">
                           Male
                         </option>
@@ -162,49 +156,56 @@ function EditProfile() {
                   </div>
                   <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
-                      <label for="Street">Password</label>
+                      <label for="Password">Password</label>
                       <input
-                        type="name"
+                        type="password"
                         class="form-control"
-                        id="Street"
-                        placeholder="Enter Street"
+                        id="Password"
+                        placeholder="Enter Password"
+                        name="password"
+                        required
+                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
+                        onChange={handleChangeFunc}
                       />
                     </div>
                   </div>
                   <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                     <div class="form-group">
-                      <label for="ciTy">Confirm Password</label>
+                      <label for="ConfirmPassword">Confirm Password</label>
                       <input
-                        type="name"
+                        type="password"
                         class="form-control"
-                        id="ciTy"
-                        placeholder="Enter City"
+                        id="ConfirmPassword"
+                        placeholder="Confirm Password"
+                        name="confirmPassword"
+                        required
+                        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
+                        onChange={handleChangeFunc}
                       />
                     </div>
                   </div>
                 </div>
-                <div class="row gutters">
-                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <div class="text-right">
-                      <button
-                        type="button"
-                        id="submit"
-                        name="submit"
-                        class="btn btn-secondary"
-                      >
-                        Cancel
-                      </button>
-                      <button
-                        type="button"
-                        id="submit"
-                        name="submit"
-                        class="btn btn-primary"
-                      >
-                        Update
-                      </button>
-                    </div>
+                {error && <Alert severity="error">{error}</Alert>}
+                <div class="row gutters d-flex justify-content-center">
+                  <div class="d-flex justify-content-around updateBlockButtons">
+                    <button
+                      type="button"
+                      id="submit"
+                      name="submit"
+                      class="btn btn-secondary btn-lg"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      id="submit"
+                      name="submit"
+                      class="btn btn-primary btn-lg"
+                    >
+                      Update
+                    </button>
                   </div>
-                </div>
+              </div>
               </div>
             </div>
           </div>
